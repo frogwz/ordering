9bab71c1fa666cec1da4b3803be5ac3c15733c3c
dangyanglim <dangyanglim@qq.com>
2016-07-08 17:37:08 +0800

    ajax

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static('./'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/user', function (req, res) {
+	  res.send('Hello World!');
+});
+app.post('/order', function (req, res) {
+	console.log(req.body);
+	res.end();
+});
+
+var server = app.listen(3000, function () {
+	  var host = server.address().address;
+	    var port = server.address().port;
+
+	      console.log('Example app listening at http://%s:%s', host, port);
+});
+
---
+++ public/order.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+	$("#depart").change(function(){
+		var people=[
+			["请选择姓名"],
+			["张三","李四"],
+			["孙悟空","唐僧"],
+			["陈一","小明"]
+		];
+		$("#depart option").each(function(i){
+			$(this).click(function(){
+				var dPeople=people[i];
+				$(".name").empty();
+				for(var j=0;j<dPeople.length;j++){
+					var PeopleHtml="<option>"+dPeople[j]+"</option>";
+					$(".name").append(PeopleHtml);
+				}
+
+			});
+		});
+	});
+	$(function(){
+		var today = new Date();
+		var weekDay = ["星期天", "星期一", "星期二", "星期三", "星期四", "星期五","星期六"];
+		var text=today.getMonth();
+		var year=today.getFullYear();
+		var Month=parseInt(text)+1;
+		var days=30;
+		if(Month==2){
+			days=year%4==0?29:28;
+
+
+
+		}else if((Month==1)||(Month==3)||(Month==5)||(Month==7)||(Month==8)||(Month==10)||(Month==12)){
+			days=31;
+		}else{
+			days=30;
+		}
+		for(var i=1;i<days+1;i++){
+			text=Month.toString();
+			var ddate=i.toString();
+			text=year+"-"+text+"-"+ddate;
+			//var t=$("tr").children().eq(4).text();
+			$("tr").children().eq(i*4).html(text);
+			var week=new Date(text).getDay();
+			$("tr").children().eq(i*4+1).html(weekDay[week]);
+			if(week==0){
+				$("tr").children().eq(i*4+0).css("background-color","#bbb");
+				$("tr").children().eq(i*4+1).css("background-color","#bbb");
+				$("tr").children().eq(i*4+2).css("background-color","#bbb");
+				$("tr").children().eq(i*4+3).css("background-color","#bbb");
+			}
+			var input="<input type='checkbox' name='lunch' value='"+ddate+"'>";
+			$("tr").children().eq(i*4+2).html(input);
+			input="<input type='checkbox' name='dinner' value='"+ddate+"'>";
+			$("tr").children().eq(i*4+3).html(input);
+
+			//alert(t);
+		}
+
+	});
+	$("#order").click(function() {
+		$.ajax({
+			type: 'post',
+			url: 'http://139.129.34.99:3000/order',
+			data: $("form").serialize(),
+			//async:false,
+			success: function(data) {
+				// your code
+			}
+		});
+		return false;
+	});
+	//	$("#order").click(function(){
+	//		alert("ok");
+	//	});
+
+
+})
